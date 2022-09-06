0381bf7feb54b6c693af16747a15535410d1020a
David Walker <thewalker.david@gmail.com>
2022-09-06 18:37:35 -0400

    Added web scraping crypto script

---
+++ WebScraping/crypto.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+"""
+Tutorial following Tech With Time vide:
+
+Beautiful Soup 4 Tutorial #3 - Navigating The HTML Tree
+
+Link:
+https://www.youtube.com/watch?v=lC6mucyD17k
+"""
+__author__ = "David Walker"
+__version__ = "Web Scraping Tutorial"
+__pylint__ = "2.12.2"
+
+
+if __name__ == '__main__':
+
+    url = "https://coinmarketcap.com/"
+    result = requests.get(url).text
+    doc = BeautifulSoup(result, "html.parser")
+
+    tbody = doc.tbody
+    
+    trs = tbody.contents
+
+    prices = {}
+
+    for tr in trs:
+        name, price = tr.contents[2:4]
+        try:
+            fixed_name = name.p.string
+            fixed_price = price.a.string
+        except AttributeError:
+            test = tr.findChildren()[4].findChildren()[2]
+            fixed_name = test.string
+            fixed_price = tr.findChildren()[-3].text
+        prices[fixed_name] = fixed_price
+
+    print(prices)
